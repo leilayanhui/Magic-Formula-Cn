ca7dd212fbc20c7868fa5ba9beb8b96f5752b5b5
Yan Hui <leilayanhui@sina.com>
2017-03-17 11:51:15 +0800

    update basicTable
    将总市值直接导入到 basicTable 中

--- Model.py
+++ Model.py
@@ -49,23 +49,23 @@ class BasicTable(db.Model):
     code = db.Column(db.String(64)) #代码
     name = db.Column(db.String(64)) #名称
     industry = db.Column(db.String(64)) #所属行业
-    income1 = db.Column(db.Integer) #第1季度净利润
-    income2 = db.Column(db.Integer) #第2季度净利润
-    income3 = db.Column(db.Integer) #第3季度净利润
-    income4 = db.Column(db.Integer) #第4季度净利润
-    incometax1 = db.Column(db.Integer) #第1季度所得税
-    incometax2 = db.Column(db.Integer) #第2季度所得税
-    incometax3 = db.Column(db.Integer) #第3季度所得税
-    incometax4 = db.Column(db.Integer) #第4季度所得税
-    finanExp1 = db.Column(db.Integer) #第1季度财务费用
-    finanExp2 = db.Column(db.Integer) #第2季度财务费用
-    finanExp3 = db.Column(db.Integer) #第3季度财务费用
-    finanExp4 = db.Column(db.Integer) #第4季度财务费用
+    income1 = db.Column(db.Integer) #1季度净利润
+    income2 = db.Column(db.Integer) #2季度净利润
+    income3 = db.Column(db.Integer) #3季度净利润
+    income4 = db.Column(db.Integer) #4季度净利润
+    incometax1 = db.Column(db.Integer) #1季度所得税
+    incometax2 = db.Column(db.Integer) #2季度所得税
+    incometax3 = db.Column(db.Integer) #3季度所得税
+    incometax4 = db.Column(db.Integer) #4季度所得税
+    finanExp1 = db.Column(db.Integer) #1季度财务费用
+    finanExp2 = db.Column(db.Integer) #2季度财务费用
+    finanExp3 = db.Column(db.Integer) #3季度财务费用
+    finanExp4 = db.Column(db.Integer) #4季度财务费用
     THEquity = db.Column(db.Integer) #所有者权益合计
     iAssets = db.Column(db.Integer) #无形资产
     goodwill = db.Column(db.Integer) #商誉
     totals = db.Column(db.Integer) #总股本
-    close = db.Column(db.Integer) #收盘价
+    mktcap = db.Column(db.Integer) #总市值
 
     def __repr__(self):
         return '<basicTable %r>' % self.name
@@ -91,7 +91,6 @@ class TodayTable(db.Model):
     mktcap = db.Column(db.Integer) #总市值
     nmc = db.Column(db.Integer) #流通市值
 
-
     def __repr__(self):
         return '<basicTable %r>' % self.name
 
@@ -114,18 +113,18 @@ class BasicModel(object):
             record = BasicTable(code = line[1],
                                 name = line[2],
                                 industry = line[3],
-                                income1 = line[4], #第1季度净利润
-                                income2 = line[5], #第2季度净利润
-                                income3 = line[6], #第3季度净利润
-                                income4 = line[7], #第4季度净利润
-                                incometax1 = line[8], #第1季度所得税
-                                incometax2 = line[9], #第2季度所得税
-                                incometax3 = line[10], #第3季度所得税
-                                incometax4 = line[11], #第4季度所得税
-                                finanExp1 = line[12], #第1季度财务费用
-                                finanExp2 = line[13], #第2季度财务费用
-                                finanExp3 = line[14], #第3季度财务费用
-                                finanExp4 = line[15], #第4季度财务费用
+                                income1 = line[4], #1季度净利润
+                                income2 = line[5], #2季度净利润
+                                income3 = line[6], #3季度净利润
+                                income4 = line[7], #4季度净利润
+                                incometax1 = line[8], #1季度所得税
+                                incometax2 = line[9], #2季度所得税
+                                incometax3 = line[10], #3季度所得税
+                                incometax4 = line[11], #4季度所得税
+                                finanExp1 = line[12], #1季度财务费用
+                                finanExp2 = line[13], #2季度财务费用
+                                finanExp3 = line[14], #3季度财务费用
+                                finanExp4 = line[15], #4季度财务费用
                                 THEquity = line[16], #所有者权益合计
                                 iAssets = line[17], #无形资产
                                 goodwill = line[18], #商誉
@@ -133,28 +132,38 @@ class BasicModel(object):
             db.session.add(record)
             db.session.commit()
 
-    def update(self):
+    def updateReport(self):
         table = getData(self.filename,self.sheet)
         nrows = table.nrows
         for i in range(3,nrows):
             line = table.row_values(i)
             record = BasicTable.query.filter_by(code = line[1]).first()
             if record is not None:
-                record.income1 = line[4] #第1季度净利润
-                record.income2 = line[5] #第2季度净利润
-                record.income3 = line[6] #第3季度净利润
-                record.income4 = line[7] #第4季度净利润
-                record.incometax1 = line[8] #第1季度所得税
-                record.incometax2 = line[9] #第2季度所得税
-                record.incometax3 = line[10] #第3季度所得税
-                record.incometax4 = line[11] #第4季度所得税
-                record.finanExp1 = line[12] #第1季度财务费用
-                record.finanExp2 = line[13] #第2季度财务费用
-                record.finanExp3 = line[14] #第3季度财务费用
-                record.finanExp4 = line[15] #第4季度财务费用
+                record.income1 = line[4] #1季度净利润
+                record.income2 = line[5] #2季度净利润
+                record.income3 = line[6] #3季度净利润
+                record.income4 = line[7] #4季度净利润
+                record.incometax1 = line[8] #1季度所得税
+                record.incometax2 = line[9] #2季度所得税
+                record.incometax3 = line[10] #3季度所得税
+                record.incometax4 = line[11] #4季度所得税
+                record.finanExp1 = line[12] #1季度财务费用
+                record.finanExp2 = line[13] #2季度财务费用
+                record.finanExp3 = line[14] #3季度财务费用
+                record.finanExp4 = line[15] #4季度财务费用
                 db.session.add(record)
                 db.session.commit()
 
+    def updateToday(self):
+        table = getData(self.filename,self.sheet)
+        nrows = table.nrows
+        for i in range(2,nrows):
+            line = table.row_values(i)
+            record = BasicTable.query.filter_by(code = line[1]).first()
+            if record is not None:
+                record.mktcap = line[14] #总市值
+                db.session.add(record)
+                db.session.commit()
 
 class TodayModel(object):
     '''日报数据入库模块'''
@@ -190,6 +199,7 @@ if __name__ == '__main__':
     db.drop_all() #删除所有表
     db.create_all() #新建所有表
     BasicModel('basic.xls',u'selResult').insert()
+    BasicModel('today.xls',u'Sheet1').updateToday()
     TodayModel('today.xls',u'Sheet1').insert()
     record = BasicTable.query.filter_by(code = '300151').first()
     a = record.finanExp1
